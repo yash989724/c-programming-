f550de2a40e72c743729baebd1eebf091d8498ff
yash989724 <yashk989724@gmail.com>
2024-09-27 12:00:28 +0530

    Add files via upload

---
+++ SIZEOF.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   printf("size of int :%u", (sizeof(int)));
+   printf("size of float:%u", (sizeof(float)));
+   printf("size of char:%u",(sizeof(char)));
+   printf("size of double:%u",(sizeof(double)));
+   getch();
+   }
